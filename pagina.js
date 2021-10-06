2600d2cb29a67e62e382a981ff1e7beb2a338849
Igorrosiak <igor_rosiak@estudante.sc.senai.br>
2021-10-06 20:55:20 -0300

    v1

---
+++ pagina.js
@@ -0,0 +1,11 @@
+const irexpprofissional = function(){
+    window.location.href = "experienciaprofissional.html"
+}
+
+const irformação = function(){
+    window.location.href = "formação.html"
+}
+
+const irportfolio = function (){
+    window.location.href = "portfólio.html"
+}
